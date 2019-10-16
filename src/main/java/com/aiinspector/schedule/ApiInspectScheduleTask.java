707271b07af5f31d20a4fac23b02e8e4b8519f0a
joey <joey.csnt@gmail.com>
2019-10-16 12:15:52 +0800

    rename schedule task

--- src/main/java/com/aiinspector/schedule/ApiInspectScheduleTask.java
+++ src/main/java/com/aiinspector/schedule/ApiInspectScheduleTask.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class CheckScheduleTask {
+public class ApiInspectScheduleTask {
 
 	@Autowired
 	CheckSatusService checkSatusServiceImp;
